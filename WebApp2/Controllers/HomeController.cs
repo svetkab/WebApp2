21d431143cce968f9c21973cee47e8d405c88f6d
Yury Brodskiy <ybrodskiy@fanatics.com>
2022-05-27 16:13:21 -0700

    Mis=gration complete (bad CSS)

--- WebApp2/Controllers/HomeController.cs
+++ WebApp2/Controllers/HomeController.cs
@@ -1,26 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+
 
 namespace WebApp2.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController : Microsoft.AspNetCore.Mvc.Controller
     {
-        public ActionResult Index()
+        public Microsoft.AspNetCore.Mvc.ActionResult Index()
         {
             return View();
         }
 
-        public ActionResult About()
+        public Microsoft.AspNetCore.Mvc.ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
 
             return View();
         }
 
-        public ActionResult Contact()
+        public Microsoft.AspNetCore.Mvc.ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
 
